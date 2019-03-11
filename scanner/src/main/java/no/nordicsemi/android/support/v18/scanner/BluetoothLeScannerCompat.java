2ce683851cc3facdeec42671f8dc0bb882910888
Paul Peavyhouse <pv@swooby.com>
2019-03-11 16:08:21 -0700

    Oops; missed an important line :)

--- scanner/src/main/java/no/nordicsemi/android/support/v18/scanner/BluetoothLeScannerCompat.java
+++ scanner/src/main/java/no/nordicsemi/android/support/v18/scanner/BluetoothLeScannerCompat.java
@@ -26,6 +26,7 @@ import android.Manifest;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.os.Build;
+import android.os.Build.VERSION;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.SystemClock;
@@ -89,7 +90,7 @@ public abstract class BluetoothLeScannerCompat {
 
 	private static BluetoothLeScannerCompat instance;
 
-	private static int versionSdkInt;
+	private static int versionSdkInt = VERSION.SDK_INT;
 
 	public static void setScannerApi(int versionSdkInt) {
         BluetoothLeScannerCompat.versionSdkInt = versionSdkInt;
